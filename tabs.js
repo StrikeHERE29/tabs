55b3bd04f6707315dc238a6ced0128ba7ecb0918
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-03-23 14:18:06 +0200

    added another object in html added functionality in js to switch between objects

--- tabs.js
+++ tabs.js
@@ -1,26 +1,42 @@
 let btns = document.querySelectorAll(".buttons");
 let boardText = document.querySelectorAll(".board");
-let infoContainer = document.querySelector(".about");
+let infoContainers = document.querySelectorAll(".about, .another-about");
+let right = document.querySelector("#right");
+let left = document.getElementById("left");
+let aboutCenters = document.querySelectorAll(".about-center")
 
-infoContainer.addEventListener("click", function(e){
-    let id = e.target.dataset.id;
-    
-    
-    //switch on click the background color for buttons
-    if(id){
-        
-        btns.forEach(function(btn){
-            btn.classList.remove("active");
-            e.target.classList.add("active")
-        })
-        // remove the text from the board
-        boardText.forEach(function(board){
-            board.classList.remove("active");
+infoContainers.forEach(function(infoContainer) {
+    infoContainer.addEventListener("click", function(e) {
+        let id = e.target.dataset.id;
 
-            // add the text for the board
-        const element = document.getElementById(id);
-        element.classList.add("active");
-        })
-       
-    }
-})
+        // Switch on click to change background color for buttons
+        if (id) {
+            btns.forEach(function(btn) {
+                btn.classList.remove("active");
+            });
+            e.target.classList.add("active");
+
+            // Remove the text from board
+            boardText.forEach(function(board) {
+                board.classList.remove("active");
+            });
+
+            // Add the text for the specific board
+            let element = document.getElementById(id);
+            if (element) {
+                element.classList.add("active");
+            }
+        }
+    });
+});
+
+
+right.addEventListener("click", function(e) {
+    aboutCenters.forEach(function(aboutCenter) {
+        if (!aboutCenter.classList.contains("active")) {
+            aboutCenter.classList.add("active");
+        } else {
+            aboutCenter.classList.remove("active");
+        }
+    });
+});
