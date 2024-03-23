cd1aa0d20863c0f8d11a40f8b12ee5f71ac5a818
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-03-23 13:28:36 +0200

    functionality to add/remove text for board

--- tabs.js
+++ tabs.js
@@ -4,12 +4,23 @@ let infoContainer = document.querySelector(".about");
 
 infoContainer.addEventListener("click", function(e){
     let id = e.target.dataset.id;
+    
+    
+    //switch on click the background color for buttons
     if(id){
+        
         btns.forEach(function(btn){
             btn.classList.remove("active");
             e.target.classList.add("active")
         })
-        
+        // remove the text from the board
+        boardText.forEach(function(board){
+            board.classList.remove("active");
+
+            // add the text for the board
+        const element = document.getElementById(id);
+        element.classList.add("active");
+        })
        
     }
 })
